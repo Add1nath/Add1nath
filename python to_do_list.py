8e28f8ce6e7440d35330e37eb9f7e88f61ce845a
Add1nath <138708110+Add1nath@users.noreply.github.com>
2023-07-06 09:25:01 +0530

    Update python to_do_list.py
    Application greets when screen unlocks and show to do list, on lock screen.

--- python to_do_list.py
+++ python to_do_list.py
@@ -1,10 +1,33 @@
-- 👋 Hi I’m @Add1nath
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
 
-<!---
-Add1nath/Add1nath is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+import android
+
+def on_unlock(context):
+    print("Hello!")
+
+    to_do_list = ["Buy groceries", "Do laundry", "Call mom"]
+
+    def show_to_do_list():
+        print(to_do_list)
+
+    def edit_to_do_list():
+        print("Edit to-do list")
+
+    button = android.Button(context)
+    button.setOnClickListener(show_to_do_list)
+    button.setOnLongClickListener(lambda view: print("Long click"))
+    button.setX(0)
+    button.setY(0)
+    button.setText("To-Do List")
+    button.show()
+
+    edit_button = android.Button(context)
+    edit_button.setOnClickListener(edit_to_do_list)
+    edit_button.setX(-50)
+    edit_button.setY(0)
+    edit_button.setText("Edit")
+    edit_button.show()
+
+android.register_callback(android.EVENT_SCREEN_ON, on_unlock)
+
+while True:
+    android.sleep(1)
